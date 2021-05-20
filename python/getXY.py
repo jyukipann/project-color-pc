739116448557dd8c3b013f4b9383f040a9102c2a
JUKI <jyukipann@yahoo.co.jp>
2021-05-20 17:03:04 +0900

    pcd

--- python/getXY.py
+++ python/getXY.py
@@ -4,14 +4,9 @@ import cv2
 def printCoor(event,x,y,flags,param):
 	if event == cv2.EVENT_LBUTTONDOWN:
 		print(x, y)
-img = cv2.imread('gazou.bmp')
+img = cv2.imread('1597739520_490239122.jpg')
 #画像のウインドウに名前をつけ、コールバック関数をセット
-cv2.namedWindow('image')
+cv2.imshow('image',img)
 cv2.setMouseCallback('image',printCoor)
-cv2.moveWindow('image', 100, 200)
-while(1):
-	cv2.imshow('image',img)
-	#ESCキーでブレーク
-	if cv2.waitKey(20) & 0xFF == 27:
-		break
+cv2.waitKey(0)
 cv2.destroyAllWindows()
